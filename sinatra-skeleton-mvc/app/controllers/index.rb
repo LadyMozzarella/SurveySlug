efef08856e21ab63e0ffa188eacf9fc1d7b990c4
Irene Yiu <apprentice@dbc02.local>
2014-02-28 15:51:38 -0800

    Add fix for lost of question reference when posting to /surveys

--- sinatra-skeleton-mvc/app/controllers/index.rb
+++ sinatra-skeleton-mvc/app/controllers/index.rb
@@ -6,19 +6,18 @@ get '/' do
 end
 
 ## This is the route/page to create the survey
-get '/survey/new' do
+get '/surveys/new' do
   erb :create_survey
 end
 
 ## This will add the survey that the user created to the DB
 post '/surveys' do
-  puts "-------------------------"
   @survey = Survey.create(name: params[:name], user_id: current_user)
-  @survey.questions.create(prompt: params[:prompt])
+  @question = @survey.questions.create(prompt: params[:prompt])
   params[:option].each do |option|
     @question.options.create(name: option)
   end
-
+  redirect "/surveys/#{@survey.id}"
 end
 
 ## This is a placeholder for the survey link
