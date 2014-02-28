41026266fb02f870ec891af6cbff3b54e1ba8129
Irene Yiu <apprentice@dbc02.local>
2014-02-28 14:22:21 -0800

    Add has_many relations to the models.

--- sinatra-skeleton-mvc/app/models/option.rb
+++ sinatra-skeleton-mvc/app/models/option.rb
@@ -1,4 +1,5 @@
 class Option < ActiveRecord::Base
   validates_presence_of :name
   belongs_to :question
+  has_many :responses
 end
--- sinatra-skeleton-mvc/app/models/user.rb
+++ sinatra-skeleton-mvc/app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
   validates_presence_of :name, :email, :password
+  has_many :surveys
+  has_many :responses
 end
