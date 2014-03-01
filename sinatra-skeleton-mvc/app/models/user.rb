06730448be6037af7bfb2033f4252a1e3f011f1e
thefenry <thefenry@gmail.com>
2014-03-01 11:42:55 -0800

    Add encryption and validation methods for password.

--- sinatra-skeleton-mvc/app/models/user.rb
+++ sinatra-skeleton-mvc/app/models/user.rb
@@ -1,5 +1,16 @@
+require 'bcrypt'
 class User < ActiveRecord::Base
   validates_presence_of :name, :email, :password
   has_many :surveys
   has_many :responses
+
+  before_save :encrypt_password
+
+  def encrypt_password
+  	self.password = Bcrypt::Password.create(self.password)
+  end
+
+  def authenticate_password(password)
+  	Bcrypt::Password.new(self.password) == password
+  end
 end
