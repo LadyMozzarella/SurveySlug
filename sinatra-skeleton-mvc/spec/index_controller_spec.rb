47c6399d1b30df81cf6b78f106802247cf269d78
Harry Whelchel <apprentice@dbc04.local>
2014-03-07 14:47:55 -0800

    Fix index controller test by passing session id into get request.
    With the session id the user is logged in and the appropriate page
    renders.

--- sinatra-skeleton-mvc/spec/index_controller_spec.rb
+++ sinatra-skeleton-mvc/spec/index_controller_spec.rb
@@ -2,9 +2,13 @@ require 'spec_helper'
 
 describe "IndexController" do
   let(:user) { User.create(name: 'bobo', email: 'bob@dbc.com', password: '1234') }
-  describe 'get all surveys' do
+  describe 'when logged in get all surveys' do
     it 'should display all the surveys for a user' do
-      get "/users/#{user.id}/surveys"
+      session = {
+        'rack.session' => { id: 1}
+      }
+      get "/users/#{user.id}/surveys","",session
+      p last_response
       expect(last_response).to be_ok
     end
   end
@@ -13,34 +17,11 @@ describe "IndexController" do
     let(:survey) { Survey.create(name: 'My Survey', user_id: user.id) }
     it 'should display page to answer survey' do
       get "/surveys/#{survey.id}"
+      p last_response.body
       expect(last_response.body).to include('Surveys')
     end
   end
 
-  # describe 'thank you page' do
-  #   before(:all) do
-  #     ivan = User.create(name: "Ivan", email: "is@gmail.com", password: "password")
-  #     post '/login', {user_id: ivan.id, name: ivan.name, email: ivan.email, password: ivan.password}
-  #   end
-
-  #   it 'should exist' do
-  #     p last_response
-  #     get '/thankyou'
-  #     expect(last_response).to be_ok
-  #   end
-  # end
-
-  # Same goes for this RSPEC test.
-  # describe 'create survey' do
-  #   let(:user){User.create(name: "Darren", email: "dg@gmail.com", password: "password")}
-  #   let(:survey){Survey.create(name: "Survey", user_id: user.id)}
-  #   let(:question){Question.create(prompt: "Question", survey_id: survey.id)}
-  #   let(:option1){Option.create(name: "Option1", question_id: question.id)}
-  #   let(:option2){Option.create(name: "Option2", question_id: question.id)}
-
-  #   it 'should create new survey with questions and options' do
-  #     post '/surveys', {user_id: user.id, name: survey.name, prompt: question.prompt, option: [option1.name, option2.name]}
-  #     expect(last_response).to be_ok
-  #   end
-  # end
 end
+
+
